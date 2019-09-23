8a0a69e10b30c24fad11e4fb5054143b301ddb68
Fox <36572168+Fox520@users.noreply.github.com>
2019-09-23 17:59:28 +0200

    add author procedure [not tested]

---
+++ SQL/procedures/spAddAuthor.sql
@@ -0,0 +1,15 @@
+USE SchoolProject
+CREATE PROCEDURE spAddAuthor
+@af_name varchar(100),
+@title varchar(4),
+@fname varchar(20),
+@lname varchar(20)
+AS
+BEGIN
+	DECLARE @scopeID int
+	insert into AFFILIATION(@affiliation_name) values(@af_name)
+	SET @scopeID = SCOPE_IDENTITY()
+	insert into TITLE(title) values(@title)
+	insert into NAMES(first_name, last_name) values(@fname, @lname)
+	insert into AUTHOR(title_id, name_id, affiliation_id) values( @scopeID, @scopeID, @scopeID)
+END
