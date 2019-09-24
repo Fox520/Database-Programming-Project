474bdea58402cb0ee9f8ef64cd5dc48123c5e5d4
Fox <36572168+Fox520@users.noreply.github.com>
2019-09-24 13:28:04 +0200

    Update spAddAuthor.sql

--- SQL/procedures/spAddAuthor.sql
+++ SQL/procedures/spAddAuthor.sql
@@ -1,4 +1,5 @@
 USE SchoolProject
+Go
 CREATE PROCEDURE spAddAuthor
 @af_name varchar(100),
 @title varchar(4),
@@ -7,7 +8,7 @@ CREATE PROCEDURE spAddAuthor
 AS
 BEGIN
 	DECLARE @scopeID int
-	insert into AFFILIATION(@affiliation_name) values(@af_name)
+	insert into AFFILIATION(affiliation_name) values(@af_name)
 	SET @scopeID = SCOPE_IDENTITY()
 	insert into TITLE(title) values(@title)
 	insert into NAMES(first_name, last_name) values(@fname, @lname)
