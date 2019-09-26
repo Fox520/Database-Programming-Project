633cdd30be9d17fb9e90a4ea69ef96987e826563
Wombola <54849261+Wombola@users.noreply.github.com>
2019-09-26 21:00:04 +0200

    Update requested procedures.sql
    couldn't figure the switch statement, will do that later tonight

--- SQL/procedures/requested procedures.sql
+++ SQL/procedures/requested procedures.sql
@@ -6,35 +6,6 @@ AS
 SELECT * FROM Author
 GO;
 
--- get publications by certain author
-
-
-CREATE PROCEDURE getAllPublications
-@Author nvarchar(30)
-AS
-SELECT * FROM Publications WHERE Author = @Author
-GO;
-
--- get file path (if any) of a publication
-
-
-CREATE PROCEDURE getAllFile
-@Publication nvarchar(30)
-AS
-SELECT * FROM File WHERE Publication = @Publication
-GO;
-
--- get publication details (the fields)
-
-
-CREATE PROCEDURE getAllPublication
-@City nvarchar(30),
-@File nvarchar(30),
-@Publisher nvarchar(30)
-AS
-SELECT * FROM Customers WHERE City = @City AND File = @File AND @Publisher = Publisher
-GO;
-
 -- get all affiliations in database
 
 
@@ -42,7 +13,13 @@ CREATE PROCEDURE getAllAffiliations
 AS
 SELECT * FROM Affiliation
 GO;
+-- get all Names in database
 
+
+CREATE PROCEDURE getAllNames
+AS
+SELECT * FROM Names
+GO;
 
 -- get all titles in database
 
@@ -51,12 +28,61 @@ AS
 SELECT * FROM Title
 GO;
 
+-- get all publications information
+
+
+CREATE PROCEDURE getAllPublications
+AS
+SELECT * FROM Publication
+GO;
+
+
+-- get publications by certain author
+
+
+CREATE PROCEDURE getAllPublicationAuthor
+@Author nvarchar(30)
+AS
+SELECT * FROM Publication WHERE Author = @Author
+GO;
+
+-- get file path (if any) of a publication
+
+
+CREATE PROCEDURE getAllFile
+@Publication nvarchar(30)
+AS
+SELECT * FROM Publication WHERE File = @File
+GO;
+
 
 -- get publications from certain city
 
-CREATE PROCEDURE getAllPublications
+CREATE PROCEDURE getAllPublicationCity
 @City nvarchar(30)
 AS
-SELECT * FROM Publications WHERE City = @City
+SELECT * FROM Publication WHERE City = @City
 GO;
+
+
+-- get publications from certain publishers
+
+CREATE PROCEDURE getAllPublicationPublisher
+@Publisher nvarchar(30)
+AS
+SELECT * FROM Publication WHERE Publisher = @Publisher
+GO;
+
+
+-- get publication details (the fields)
+
+
+CREATE PROCEDURE getAllPublicationDetail
+@City nvarchar(30),
+@File nvarchar(30),
+@Publisher nvarchar(30)
+AS
+SELECT * FROM Publication WHERE City = @City AND File = @File AND @Publisher = Publisher
+GO;
+
 
