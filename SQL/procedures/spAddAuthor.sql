999eec0c0e906864adcff30f55bdeee452ccd071
Fox <36572168+Fox520@users.noreply.github.com>
2019-09-27 20:34:57 +0200

    Update spAddAuthor.sql

--- SQL/procedures/spAddAuthor.sql
+++ SQL/procedures/spAddAuthor.sql
@@ -1,16 +1,26 @@
 USE SchoolProject
 Go
 CREATE PROCEDURE spAddAuthor
-@af_name varchar(100),
-@title varchar(4),
+@affiliation_id varchar(100),
+@title_id varchar(4),
 @fname varchar(20),
 @lname varchar(20)
 AS
 BEGIN
-	DECLARE @scopeID int
-	insert into AFFILIATION(affiliation_name) values(@af_name)
-	SET @scopeID = SCOPE_IDENTITY()
-	insert into TITLE(title) values(@title)
-	insert into NAMES(first_name, last_name) values(@fname, @lname)
-	insert into AUTHOR(title_id, name_id, affiliation_id) values( @scopeID, @scopeID, @scopeID)
+	IF @fname IS NULL OR @lname IS NULL
+	BEGIN
+		;THROW 99001, 'Provide first/last name', 1;
+		RETURN
+	END
+	IF @affiliation_id IS NULL
+	BEGIN
+		;THROW 99001, 'Provide an affiliation_id', 1;
+		RETURN
+	END
+	IF @title_id IS NULL
+	BEGIN
+		;THROW 99001, 'Provide a title_id', 1;
+		RETURN
+	END
+	insert into AUTHOR(title_id, first_name, last_name, affiliation_id) values( @title_id, @fname, @lname, @affiliation_id)
 END
