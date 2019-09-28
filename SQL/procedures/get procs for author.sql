b549ef987448530b9d202c571628d8cb9b2af7a3
Fox <36572168+Fox520@users.noreply.github.com>
2019-09-28 15:33:24 +0200

    fix procedure

--- SQL/procedures/get procs for author.sql
+++ SQL/procedures/get procs for author.sql
@@ -4,10 +4,10 @@ CREATE PROCEDURE spGetAuthorInformation
 AS
 BEGIN
 	SET NOCOUNT ON;
-	SELECT AUTHOR.author_id, AUTHOR.first_name, AFFILIATION.affiliation_name, TITLE.title from AUTHOR
+	SELECT AUTHOR.author_id, AUTHOR.first_name, AFFILIATION.affiliation_id, AFFILIATION.affiliation_name, TITLE.title_id, TITLE.title from AUTHOR
 			JOIN AFFILIATION ON AUTHOR.affiliation_id = AFFILIATION.affiliation_id
 			JOIN TITLE ON AUTHOR.title_id = TITLE.title_id
-			WHERE AFFILIATION.affiliation_id = AUTHOR.affiliation_id AND TITLE.title_id = AUTHOR.author_id
+			WHERE AFFILIATION.affiliation_id = AUTHOR.affiliation_id AND TITLE.title_id = AUTHOR.title_id
 			
 	FOR XML RAW('author'), ROOT('authors'), ELEMENTS
 END
