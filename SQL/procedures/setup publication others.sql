b97d983e6bb69d277d1a1ca76fc18d91bc58e7b3
Fox <36572168+Fox520@users.noreply.github.com>
2019-09-28 18:13:04 +0200

    Update setup publication others.sql
    update/delete city, publisher
    delete publication

--- SQL/procedures/setup publication others.sql
+++ SQL/procedures/setup publication others.sql
@@ -1,4 +1,3 @@
--- testing
 USE SchoolProject
 GO
 CREATE PROCEDURE spAddPublisher
@@ -51,6 +50,193 @@ BEGIN
 		END
 
 		insert into CITY(city_name) values(@city_name)
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_STATE() AS ErrorState,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage
+		FOR XML RAW('error'), ROOT('errors'), ELEMENTS
+	END CATCH
+END
+
+GO
+CREATE PROCEDURE spUpdateCity
+@city_name VARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON
+	BEGIN TRY
+		IF @city_name IS NULL OR @city_name = ''
+		BEGIN
+			;THROW 99001, 'Provide city name', 1;
+			RETURN
+		END
+		IF NOT EXISTS(SELECT city_name FROM CITY WHERE city_name = @city_name)
+		BEGIN
+			;THROW 99001, 'City does not exist', 1;
+			RETURN
+		END
+		IF @city_name LIKE '%[0-9]%'
+		BEGIN
+			;THROW 99001, 'Only letters allowed for city name', 1;
+			RETURN
+		END
+		UPDATE CITY
+		SET city_name = @city_name
+		WHERE city_name = @city_name
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_STATE() AS ErrorState,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage
+		FOR XML RAW('error'), ROOT('errors'), ELEMENTS
+	END CATCH
+END
+
+GO
+CREATE PROCEDURE spUpdatePublisher
+@publisher_name VARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON
+	BEGIN TRY
+		IF @publisher_name IS NULL OR @publisher_name = ''
+		BEGIN
+			;THROW 99001, 'Provide a publisher name', 1;
+			RETURN
+		END
+		IF NOT EXISTS(SELECT publisher_name FROM PUBLISHER WHERE publisher_name = @publisher_name)
+		BEGIN
+			;THROW 99001, 'Publisher does not exist', 1;
+			RETURN
+		END
+		IF @publisher_name LIKE '%[0-9]%'
+		BEGIN
+			;THROW 99001, 'Only letters allowed for publisher name', 1;
+			RETURN
+		END
+		UPDATE PUBLISHER
+		SET publisher_name = @publisher_name
+		WHERE publisher_name = @publisher_name
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_STATE() AS ErrorState,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage
+		FOR XML RAW('error'), ROOT('errors'), ELEMENTS
+	END CATCH
+END
+
+GO
+CREATE PROCEDURE spDeletePublisher
+@publisher_name VARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON
+	BEGIN TRY
+		IF @publisher_name IS NULL OR @publisher_name = ''
+		BEGIN
+			;THROW 99001, 'Provide a publisher name', 1;
+			RETURN
+		END
+		IF NOT EXISTS(SELECT publisher_name FROM PUBLISHER WHERE publisher_name = @publisher_name)
+		BEGIN
+			;THROW 99001, 'Publisher does not exist', 1;
+			RETURN
+		END
+		IF @publisher_name LIKE '%[0-9]%'
+		BEGIN
+			;THROW 99001, 'Only letters allowed for publisher name', 1;
+			RETURN
+		END
+		DELETE FROM PUBLISHER
+		WHERE publisher_name = @publisher_name
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_STATE() AS ErrorState,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage
+		FOR XML RAW('error'), ROOT('errors'), ELEMENTS
+	END CATCH
+END
+
+GO
+CREATE PROCEDURE spDeleteCity
+@city_name VARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON
+	BEGIN TRY
+		IF @city_name IS NULL OR @city_name = ''
+		BEGIN
+			;THROW 99001, 'Provide a city name', 1;
+			RETURN
+		END
+		IF NOT EXISTS(SELECT city_name FROM CITY WHERE city_name = @city_name)
+		BEGIN
+			;THROW 99001, 'City does not exist', 1;
+			RETURN
+		END
+		IF @city_name LIKE '%[0-9]%'
+		BEGIN
+			;THROW 99001, 'Only letters allowed for city name', 1;
+			RETURN
+		END
+		DELETE FROM PUBLISHER
+		WHERE publisher_name = @city_name
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorNumber,
+			ERROR_STATE() AS ErrorState,
+			ERROR_SEVERITY() AS ErrorSeverity,
+			ERROR_PROCEDURE() AS ErrorProcedure,
+			ERROR_LINE() AS ErrorLine,
+			ERROR_MESSAGE() AS ErrorMessage
+		FOR XML RAW('error'), ROOT('errors'), ELEMENTS
+	END CATCH
+END
+
+GO
+CREATE PROCEDURE spDeletePublication
+@publication_id INT
+AS
+BEGIN
+	SET NOCOUNT ON
+	BEGIN TRY
+		IF @publication_id IS NULL OR @publication_id = ''
+		BEGIN
+			;THROW 99001, 'Provide a publication', 1;
+			RETURN
+		END
+		IF NOT EXISTS(SELECT publication_id FROM PUBLICATION WHERE publication_id = @publication_id)
+		BEGIN
+			;THROW 99001, 'Publication does not exist', 1;
+			RETURN
+		END
+		IF @publication_id LIKE '%[0-9]%'
+		BEGIN
+			;THROW 99001, 'Only letters allowed for publication', 1;
+			RETURN
+		END
+		DELETE FROM PUBLISHER
+		WHERE publisher_id = @publication_id
 	END TRY
 	BEGIN CATCH
 		SELECT
