a11ca4e5fa8ef9554fbb2285185e64d1b32886f4
Fox <36572168+Fox520@users.noreply.github.com>
2019-09-29 23:36:03 +0200

    move get all authors

--- SQL/procedures/requested procedures.sql
+++ SQL/procedures/requested procedures.sql
@@ -1,17 +1,18 @@
 USE SchoolProject
+GO
 -- get all authors
 
-CREATE PROCEDURE getAllAuthors
+CREATE PROCEDURE spGetAllAuthorsInformation
 AS
-SELECT * FROM Author
-FOR XML RAW('author'), ROOT('authors'), ELEMENTS
-Go
--- get all names
-
-CREATE PROCEDURE getAllNames
-AS
-SELECT * FROM Names
-FOR XML RAW('name'), ROOT('names'), ELEMENTS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT AUTHOR.author_id, AUTHOR.first_name, AFFILIATION.affiliation_id, AFFILIATION.affiliation_name, TITLE.title_id, TITLE.title from AUTHOR
+			JOIN AFFILIATION ON AUTHOR.affiliation_id = AFFILIATION.affiliation_id
+			JOIN TITLE ON AUTHOR.title_id = TITLE.title_id
+			WHERE AFFILIATION.affiliation_id = AUTHOR.affiliation_id AND TITLE.title_id = AUTHOR.title_id
+			
+	FOR XML RAW('author'), ROOT('authors'), ELEMENTS
+END
 GO
 -- get all affiliations
 
